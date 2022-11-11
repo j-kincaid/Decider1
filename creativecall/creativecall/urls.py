063c74a83e9a001460a36b502d96ddb8b741e2b6
Jessica Kincaid <4j.kincaid@gmail.com>
2022-11-11 16:41:37 -0600

    delete unused apps

--- creativecall/creativecall/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-app_name = "entries"
-urlpatterns = [
-    path("entries/", include("entries.urls")),
-    path("admin/", admin.site.urls),
-]
