8004961c23d66d79d2a9ed514de98fbd3a752252
Jessica Kincaid <4j.kincaid@gmail.com>
2023-01-20 14:02:14 -0600

    set up project with artworks app

--- artworks/urls.py
+++ artworks/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.artworks, name="artworks"),
+    path("artwork/<str:pk>/", views.artwork, name="artwork"),
     # path("artwork/<str:pk>/", views.artwork, name="artwork"),
     # path("create-artwork/", views.createArtwork, name="create-artwork"),
     # path("update-artwork/<str:pk>/", views.updateArtwork, name="update-artwork"),
--- artworks/views.py
+++ artworks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 # from .models import Artwork
 # from .forms import ArtworkForm
@@ -9,13 +9,13 @@ def artworks(request):
     # artworks = Artwork.objects.all()
     # context = {"artworks": artworks}
     # return render(request, "artworks/artworks.html", context)
-    return render(request, "artworks.html")
+    return render(request, "artworks/artworks.html")
 
 
 
 # http://127.0.0.1:8000/artwork/1/
 def artwork(request, pk):
-    return render(request, "single-artwork.html")
+    return render(request, "artworks/single-artwork.html")
     #  
     # artworkObj = Artwork.objects.get(id=pk)
     # tags = artworkObj.tags.all()
