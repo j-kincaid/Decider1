4b9eb47dfc2b052c82e413ad1c44ffe6f2cf3836
Jessica Kincaid <4j.kincaid@gmail.com>
2023-03-07 12:52:02 -0600

    copy crit-night-official into this repo for hosting

--- artworks/models.py
+++ artworks/models.py
@@ -1,19 +1,17 @@
 from django.db import models
-
 import uuid
-# Create your models here.
+from django.db.models.deletion import CASCADE
+from panelists.models import Profile
+
 
 class Artwork(models.Model):
-    # owner = models.ForeignKey(
-    #     Profile, null=True, blank=True, on_delete=models.SET_NULL
-    # )
-    title = models.TextField(null=True, blank=True)
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=True)
     featured_image = models.ImageField(
         null=True, blank=True, default="default_image.jpg"
     )
-    year_created = models.IntegerField(default=0, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    topic = models.TextField(default="Write your goals for the work here", null=True, blank=True)
+    topic = models.TextField(default="I would like feedback on:", null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField("Tag", blank=True)
@@ -27,32 +25,50 @@ class Artwork(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ["owner", "-vote_ratio", "-vote_total", "title"]
+        # Projects display in descending order by owner value and number of reviews, then by title.
+
+    @property
+    def reviewers(self):
+        queryset = self.review_set.all().values_list("owner__id", flat=True)
+        return queryset
+
+    @property
+    def getVoteCount(self):
+        reviews = self.review_set.all()
+        upVotes = reviews.filter(value="up").count()
+        totalVotes = reviews.count()
+
+        ratio = (upVotes / totalVotes) * 100
+        self.vote_total = totalVotes
+        self.vote_ratio = ratio
+
+        self.save()
+
 
 class Review(models.Model):
-    # owner =
-    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
     VOTE_TYPE = (
-        ("one_star", 1),
-        ("two_stars", 2),
-        ("three_stars", 3),
-        ("four_stars", 4),
-        ("five_stars", 5),
+        ("up", "Up Vote"),
+        ("down", "Down Vote"),
     )
-    rating = models.CharField(max_length=200, choices=VOTE_TYPE, default="None")
-    Sum_it_up = models.CharField(max_length=400, null=True, blank=True)
-    What_works = models.TextField(null=True, blank=True)
-    What_needs_work = models.TextField(null=True, blank=True)
-    What_might_work = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
+    comments = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False
     )
 
+    class Meta:
+        unique_together = [["owner", "artwork"]]
+
     def __str__(self):
-        return self.rating
+        return self.value
 
 
-# Use Tag to create a Many to Many relationship. It connects the Artworks with the votes they receive.
+# Use Tag to create a Many to Many relationship.
 
 
 class Tag(models.Model):
