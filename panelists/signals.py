60c9db50a0202707bc0dea6bedc52a2e3b832619
Jessica Kincaid <4j.kincaid@gmail.com>
2023-03-25 16:21:24 -0500

    delete send_mail

--- panelists/signals.py
+++ panelists/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 from django.conf import settings
 
 from django.contrib.auth.models import User
